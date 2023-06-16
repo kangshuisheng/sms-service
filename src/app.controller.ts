99f27f3cdc495ba3886a5f524ca2a5f75e62b67b
kangshuisheng <2538041184@qq.com>
2023-06-16 14:45:34 +0800

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,10 @@ import { Controller, Get, Post } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { MicroserviceEvent } from 'microservice_event';
 import { AppService } from './app.service';
-
 interface SendSms {
   phone: string;
   code?: number;
 }
-
 @Controller()
 class AppController {
   constructor(private readonly appService: AppService) {}
@@ -27,5 +25,4 @@ class AppController {
     return this.appService.delete(id);
   }
 }
-
 export { AppController, SendSms };
